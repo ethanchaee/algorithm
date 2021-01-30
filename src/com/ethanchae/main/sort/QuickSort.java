b9eb63cd152fc2e97093a808bd879e3b192ef74e
EthanChae <boschae36@gmail.com>
2021-01-30 20:24:32 +0900

    퀵정렬

---
+++ src/com/ethanchae/main/sort/QuickSort.java
@@ -0,0 +1,49 @@
+package com.ethanchae.main.sort;
+
+public class QuickSort extends BaseSort {
+
+    static int n;
+
+    @Override
+    public void sort() {
+        n = getLength();
+
+        quickSort(arr, 0, n - 1);
+
+        showResult();
+    }
+
+    static void quickSort(int[] arr, int start, int end) {
+        if (end <= start) return;
+
+        int pivot = partition(arr, start, end);
+        quickSort(arr, start, pivot - 1);
+        quickSort(arr, pivot, end);
+    }
+
+    static int partition(int[] arr, int start, int end) {
+        int pivot = arr[(start + end)/2];
+        while(start < end){
+            while(arr[start] < pivot) start ++;
+            while(arr[end] > pivot) end--;
+            if(start <= end){
+                swap(arr, start, end);
+                start++;
+                end--;
+            }
+        }
+        return start;
+    }
+
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+
+    public static void main(String[] args) {
+        QuickSort quickSort = new QuickSort();
+        quickSort.sort();
+    }
+}
